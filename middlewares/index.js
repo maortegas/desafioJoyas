5165129b9398d8a6acdc59dca923bc4bf18357b3
Mauricio Ortega <mauricio.ortega.solis@gmail.com>
2024-02-13 22:18:19 -0300

    fix middleware reports

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,10 @@
 const getReportMiddleware = async (req, res, next) => {
-    const data=req.query
+   const data =req.query
     try {
-        console.log(data);
+        const report = `La url consulta es ${req.originalUrl} a tráves de metodo ${req.method}
+con los siguientes parámetros: `;
+        console.log(report);
+        console.table(data);
         next()
     } catch (error) {
         next(error)
